d0e945e7910af06a83ede2bb0791b9174f462e2d
SimmonsBI <SimmonsBI@users.noreply.github.com>
2019-01-15 23:44:12 +0000

    Update null_models.R

--- null_models.R
+++ null_models.R
@@ -2,8 +2,8 @@ library(bmotif)
 library(vegan)
 
 # generate null networks from your original network
-nm <- vegan::nullmodel(network, "curveball") 
-sm1 <- simulate(nm, nsim = 1000)
+nm <- vegan::nullmodel(network, "curveball") # set whatever null model you want here
+sm1 <- simulate(nm, nsim = 1000) # set how many null networks you want to generate
 
 # calculate motifs for all null networks
 null_motifs <- do.call("rbind", lapply(1:dim(sm1)[3], function(x) mcount(M = sm1[,,x], size_node = TRUE, normalise = FALSE)$frequency))
