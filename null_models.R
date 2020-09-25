f1f1f0ae618c24ba47da6df70a1c34940946f269
SimmonsBI <SimmonsBI@users.noreply.github.com>
2020-09-25 11:35:33 +0100

    Updated arguments for new version and typos

--- null_models.R
+++ null_models.R
@@ -6,10 +6,10 @@ nm <- vegan::nullmodel(network, "curveball") # set whatever null model you want 
 sm1 <- simulate(nm, nsim = 1000) # set how many null networks you want to generate
 
 # calculate motifs for all null networks
-null_motifs <- do.call("rbind", lapply(1:dim(sm1)[3], function(x) mcount(M = sm1[,,x], size_node = TRUE, normalise = FALSE)$frequency))
+null_motifs <- do.call("rbind", lapply(1:dim(sm1)[3], function(x) mcount(M = sm1[,,x], six_node = TRUE, normalisation = FALSE, mean_weight = FALSE, standard_dev = FALSE)$frequency))
 
 # calculate motifs for observed network
-observed_motifs <- mcount(network, size_node = TRUE, normalise = FALSE)$frequency
+observed_motifs <- mcount(network, six_node = TRUE, normalisation = FALSE, mean_weight = FALSE, standard_dev = FALSE)$frequency
 
 # calculate over/under-representation of motifs using z-scores
 (observed_motifs - apply(null_motifs, 2, "mean"))/apply(null_motifs, 2, "sd")
